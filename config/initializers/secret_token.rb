28900ba3415c1e5d88a60d307593ce74864b2b8b
leopku <leopku@qq.com>
2013-01-30 14:32:11 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebox::Application.config.secret_token = '104b4c28bcfb1a4479b9c9c07e1126b27b8f8799b2be4ce8e9754e67a945b3f0526916e4b5bb5d1cb0612d13e4baf6be8d612c0302340153b3d12266c76504c3'
